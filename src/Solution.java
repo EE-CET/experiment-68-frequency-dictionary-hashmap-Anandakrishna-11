bd9ce4959797aa11ab98ba5e3054cc7b2f890534
Anandakrishna <ellu250075@cet.ac.in>
2026-02-28 17:00:27 +0530

    added Solution

--- src/Solution.java
+++ src/Solution.java
@@ -5,21 +5,25 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        String line = scanner.nextLine().trim();
-        String[] words = line.split("\\s+");
-
-        Map<String, Integer> map = new HashMap<>();
-
+        
+        String line = scanner.nextLine();
+        
+        String[] words = line.split(" ");
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        
         for (String word : words) {
-            map.put(word, map.getOrDefault(word, 0) + 1);
-        }
-
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            System.out.print(entry.getKey() + ": " + entry.getValue());
-            if (map.size() > 1) {
-                System.out.println();
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+            } else {
+                map.put(word, 1);
             }
         }
+        
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+        scanner.close();
     }
 }
