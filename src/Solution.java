70bf77c135118e0610b5152589e14bfa52dc9f25
Anandakrishna <ellu250075@cet.ac.in>
2026-02-28 16:54:41 +0530

    Added Solution

--- src/Solution.java
+++ src/Solution.java
@@ -1,28 +1,22 @@
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         String line = scanner.nextLine();
         String[] words = line.split(" ");
-        
-        LinkedHashMap<String, Integer> map = new LinkedHashMap<>();
-        
+
+        HashMap<String, Integer> map = new HashMap<>();
+
         for (String word : words) {
-            if (map.containsKey(word)) {
-                map.put(word, map.get(word) + 1);
-            } else {
-                map.put(word, 1);
-            }
+            map.put(word, map.getOrDefault(word, 0) + 1);
         }
-        
+
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
-
-        scanner.close();
     }
 }
