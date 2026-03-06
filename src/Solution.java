0f0b3493ca4d16c805a245a255576e5468d09fd4
Anandakrishna <ellu250075@cet.ac.in>
2026-03-06 10:30:04 +0530

    Added Solution

--- src/Solution.java
+++ src/Solution.java
@@ -1,29 +1,24 @@
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        String line = scanner.nextLine();
-        
-        String[] words = line.split(" ");
-        
-        HashMap<String, Integer> map = new HashMap<>();
-        
+        	
+	String line = scanner.nextLine();
+        String[] words = line.split("\\s+");
+
+        LinkedHashMap<String, Integer> freqMap = new LinkedHashMap<>();
+
         for (String word : words) {
-            if (map.containsKey(word)) {
-                map.put(word, map.get(word) + 1);
-            } else {
-                map.put(word, 1);
-            }
-        }
-        
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
+            freqMap.put(word, freqMap.getOrDefault(word, 0) + 1);
         }
 
+        for (Map.Entry<String, Integer> entry : freqMap.entrySet()) {
+               System.out.print(entry.getKey() + ": " + entry.getValue() + " ");      }
+
         scanner.close();
+
     }
 }
